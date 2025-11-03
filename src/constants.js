305784e5e5a9ad6f779b71f5bb87be8f496d0832
Mustafa <mu88ali@gmail.com>
2025-11-03 13:03:00 +0100

    image bug fix

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,25 @@
+// Import product images
+import image1 from './assets/images/image-product-1.jpg';
+import image2 from './assets/images/image-product-2.jpg';
+import image3 from './assets/images/image-product-3.jpg';
+import image4 from './assets/images/image-product-4.jpg';
+import thumb1 from './assets/images/image-product-1-thumbnail.jpg';
+import thumb2 from './assets/images/image-product-2-thumbnail.jpg';
+import thumb3 from './assets/images/image-product-3-thumbnail.jpg';
+import thumb4 from './assets/images/image-product-4-thumbnail.jpg';
+
 export const PRODUCT_DATA = {
   id: 1,
   company: 'Sneaker Company',
   name: 'Fall Limited Edition Sneakers',
-  description: 'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.',
+  description: 'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they\'ll withstand everything the weather can offer.',
   price: 125.00,
   originalPrice: 250.00,
   discount: 50,
   images: [
-    { id: 1, full: 'src/assets/images/image-product-1.jpg', thumbnail: 'src/assets/images/image-product-1-thumbnail.jpg' },
-    { id: 2, full: 'src/assets/images/image-product-2.jpg', thumbnail: 'src/assets/images/image-product-2-thumbnail.jpg' },
-    { id: 3, full: 'src/assets/images/image-product-3.jpg', thumbnail: 'src/assets/images/image-product-3-thumbnail.jpg' },
-    { id: 4, full: 'src/assets/images/image-product-4.jpg', thumbnail: 'src/assets/images/image-product-4-thumbnail.jpg' },
+    { id: 1, full: image1, thumbnail: thumb1 },
+    { id: 2, full: image2, thumbnail: thumb2 },
+    { id: 3, full: image3, thumbnail: thumb3 },
+    { id: 4, full: image4, thumbnail: thumb4 },
   ],
 };
